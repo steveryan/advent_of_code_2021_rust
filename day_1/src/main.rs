a1f7e93e5dee2784bf5116c136385d667116f211
Steve Ryan <steveryan6989@gmail.com>
2021-12-01 09:48:12 -0500

    day 1 puzzle 2 solved

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -4,8 +4,11 @@ fn main() -> Result<(), std::io::Error>{
     let vec = contents.split("\n").collect::<Vec<&str>>();
     let mut current_depth: Option<i32> = None;
     let mut increase_count = 0;
-    for line in vec {
-        let depth = line.parse::<i32>().unwrap();
+    for (i,line) in vec.iter().enumerate() {
+        if (vec.len() - 1) < (i+2){
+            break;
+        }
+        let depth = line.parse::<i32>().unwrap() + vec[i+1].parse::<i32>().unwrap() + vec[i+2].parse::<i32>().unwrap();
         if current_depth.is_some() {
             let current = current_depth.unwrap();
             if current < depth {
