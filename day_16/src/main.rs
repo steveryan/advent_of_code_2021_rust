98f0bdebab67f19bf380942c15219b7eacde861b
Steve Ryan <steveryan6989@gmail.com>
2021-12-17 00:06:19 -0500

    can't quite get the recursion working. Calling it quits

--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -1,23 +1,35 @@
 use std::collections::HashMap;
 
 fn main() {
-    let input = "D2FE28";
+    let input = "620080001611562C8802118E34";
     let binary = get_binary(input);
-    println!("{}", binary);
     part_1(binary);
 }
 
 fn part_1(binary: String) {
-    let packet_version = from_binary(binary[0..3].to_string());
-    let packet_type = from_binary(binary[3..6].to_string());
-    let remaining = binary[6..].to_string();
-    if packet_type == "4" {
-        handle_packet_4(remaining);
-    } else {
-        handle_packet(remaining);
+    let mut version_count: i64 = 0;
+    generic_handle_packet(binary, &mut version_count);
+    println!("Part 1: {}", version_count);
+}
+
+fn generic_handle_packet(remaining: String, version_count: &mut i64) -> String {
+    println!("Called generic handle packet with: {}", remaining);
+    if remaining.len() < 3 { 
+        return "".to_string() 
     }
-    println!("Packet version: {}", packet_version);
-    println!("Packet type: {}", packet_type);
+    let mut packet_version = from_binary(remaining[0..3].to_string());
+    *version_count += packet_version.parse::<i64>().unwrap();
+    println!("version count: {}", version_count);
+    let packet_type = from_binary(remaining[3..6].to_string());
+    let mut remaining = remaining[6..].to_string();
+    
+    if packet_type == "4" {
+        remaining = handle_packet_4(remaining, version_count)
+    } else {
+        remaining = handle_packet(remaining, version_count)
+    }
+    println!("Remaining: {}", remaining);
+    return remaining;
 }
 
 fn get_binary(input: &str) -> String {
@@ -56,47 +68,73 @@ fn from_binary(input: String) -> String {
     conv.insert("101", '5');
     conv.insert("110", '6');
     conv.insert("111", '7');
-    conv.insert("0000", '0');
-    conv.insert("0001", '1');
-    conv.insert("0010", '2');
-    conv.insert("0011", '3');
-    conv.insert("0100", '4');
-    conv.insert("0101", '5');
-    conv.insert("0110", '6');
-    conv.insert("0111", '7');
-    conv.insert("1000", '8');
-    conv.insert("1001", '9');
-    conv.insert("1010", 'A');
-    conv.insert("1011", 'B');
-    conv.insert("1100", 'C');
-    conv.insert("1101", 'D');
-    conv.insert("1110", 'E');
-    conv.insert("1111", 'F');
     conv[input_str].to_string()
 }
 
-fn handle_packet_4(input: String) {
+fn handle_packet_4(input: String, mut version_count: &mut i64) -> String {
+    println!("found a literal packet: {}", input);
     let mut literal_binary = String::new();
+    let mut ending_index = 0;
     let mut inputs = input.chars()
     .collect::<Vec<char>>()
     .chunks(5)
     .map(|c| c.iter().collect::<String>())
     .collect::<Vec<String>>();
-    if inputs.last().unwrap().chars().all(|c| c == '0') {
-        inputs.remove(inputs.len() - 1);
-    } 
-    for input in &inputs {
+    for (i, input) in inputs.iter().enumerate() {
         if input.chars().collect::<Vec<char>>().first().unwrap() == &'0' {
             literal_binary.push_str(&input[1..]);
+            ending_index = i;
+            break;
         } else {
             literal_binary.push_str(&input[1..]);
+            ending_index = i;
         }
     };
-    println!("{:?}", isize::from_str_radix(&literal_binary, 2).unwrap());
+    inputs = inputs[ending_index+1..].to_vec();
+    if inputs.join("").chars().collect::<Vec<char>>().len() > 3 {
+        return inputs.join("").to_string()
+    }
+    "".to_string()
 }
 
-fn handle_packet(input: String) {
-    
+fn handle_packet(input: String, mut version_count: &mut i64) -> String {
+    println!("found an operator packet: {}", input);
+    let length_id = input[0..1].to_string();
+    let mut remaining = input[1..].to_string();
+    if length_id == "0" {
+        if remaining.chars().collect::<Vec<char>>().len() < 15 {
+            return "".to_string();
+        }
+        let bits = remaining[0..15].to_string();
+        let size = i64::from_str_radix(&bits, 2).unwrap();
+        println!("size: {}", size);
+        let end: usize = (15 + size).try_into().unwrap();
+        let mut remains = remaining[15..end].to_string();
+        let to_return = remaining[end..].to_string();
+        println!("to return: {}", to_return);
+        while remains.chars().collect::<Vec<char>>().len() > 3 {
+            remains = generic_handle_packet(remains, &mut version_count);
+        }
+            if to_return.chars().collect::<Vec<char>>().len() > 3 {
+                generic_handle_packet(to_return, &mut version_count);
+            }
+    } else {
+        if remaining.chars().collect::<Vec<char>>().len() < 11 {
+            return "".to_string();
+        }
+        let packets = remaining[0..11].to_string();
+        let num_packets = i64::from_str_radix(&packets, 2).unwrap();
+        println!("num packets: {}", num_packets);
+        remaining = remaining[11..].to_string();
+        let mut count_of_packets = 1;
+        while count_of_packets < num_packets {
+            println!("checking packet {} of {}", count_of_packets, num_packets);
+            println!("remaining: {}", remaining);
+            remaining = generic_handle_packet(remaining, &mut version_count);
+            count_of_packets += 1;
+        }
+    }
+    remaining
 }
 
 
